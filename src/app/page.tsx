979d9c05b9aa5d4c24a25a97b000d95d9006d7b3
sun <h092612@daum.net>
2025-09-26 14:58:25 +0900

    feat: 칩 공통 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,15 @@
 // src/app/page.tsx
+import Chips from '../components/Chips';
 export default function Home() {
-  return <main className="p-6">Ready 🙌</main>;
+  return (
+    <main className="p-6">
+      Ready 🙌
+      <>
+        <Chips variant="white">선택</Chips>
+        <Chips variant="blue">선택</Chips>
+        <Chips variant="gray">선택</Chips>
+        <Chips variant="orange">선택</Chips>
+      </>
+    </main>
+  );
 }
---
+++ src/components/page.tsx
@@ -0,0 +1,12 @@
+import Chips from '../components/Chips';
+
+export default function Test() {
+  return (
+    <>
+      <Chips variant="white">선택</Chips>
+      <Chips variant="blue">선택</Chips>
+      <Chips variant="gray">선택</Chips>
+      <Chips variant="orange">선택</Chips>
+    </>
+  );
+}
