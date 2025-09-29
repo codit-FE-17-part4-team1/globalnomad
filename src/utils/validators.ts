fdc3947027f591dc633c4da2a959ae7108a3cd16
김해빈 <113007354+Kimheabin@users.noreply.github.com>
2025-09-30 03:46:52 +0900

    fix: 입력값 로직 검증 validator 파일 수정

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,14 +1,16 @@
-export const validateEmail = (value: string) => {
+export const validateEmail = (email: string) => {
   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return regex.test(value) ? '' : '이메일 형식으로 작성해 주세요.';
+  return regex.test(email) ? '' : '이메일 형식으로 작성해 주세요.';
 };
 
-export const validatePassword = (value: string) => {
-  return value.length >= 8 ? '' : '8자 이상 입력해주세요.';
-};
+export function validatePassword(password: string) {
+  const passwordRegex =
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
+  return passwordRegex.test(password);
+}
 
-export const validateNickname = (value: string) => {
-  return value.length <= 10 ? '' : '열 자 이하로 작성해주세요.';
+export const validateNickname = (nickname: string) => {
+  return nickname.length <= 10 ? '' : '열 자 이하로 작성해주세요.';
 };
 
 export const validatePasswordConfirm = (password: string, confirm: string) => {
