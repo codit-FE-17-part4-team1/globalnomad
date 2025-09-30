36f949852997298e302fc0981c5d27ec5398a52b
yejinshim <simyejin9934@gmail.com>
2025-09-30 15:59:24 +0900

    chore: 반응형 breakpoint 설정

--- src/app/Profile/layout.tsx
+++ src/app/Profile/layout.tsx
@@ -1,1 +1,19 @@
-// profile card 추가할 예정 (전역으로 관리 가능하도록?)
+import ProfileCard from '@/components/Profile/ProfileCard';
+
+export default function ProfileLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex gap-6">
+      {/* 프로필 카드 */}
+      <aside>
+        <ProfileCard />
+      </aside>
+
+      {/* 각 작업한 페이지 렌더링 */}
+      <main className="flex-1">{children}</main>
+    </div>
+  );
+}
