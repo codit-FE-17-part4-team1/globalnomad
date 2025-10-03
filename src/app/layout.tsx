9c516eb1448baa9cdab125b3408d98957aed9c62
김해빈 <113007354+Kimheabin@users.noreply.github.com>
2025-10-03 22:08:47 +0900

    fix: 전역 레이아웃 조건부로 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,25 @@
-// src/app/layout.tsx
-import type { Metadata } from 'next';
-import '@/styles/global.css';
+'use client';
+
+import { usePathname } from 'next/navigation';
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
-
-export const metadata: Metadata = {
-  title: 'GlobalNomad',
-  description: 'team1',
-};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+  const hiddenRoutes = ['/', '/Login', '/Signup'];
+
+  const showLayout = !hiddenRoutes.includes(pathname);
+
   return (
     <html lang="ko">
       <body className="min-h-screen bg-background text-foreground">
-        {/* 헤더 */}
-        <Header />
-        {/* 각 페이지 */}
+        {showLayout && <Header />}
         <main className="mx-auto">{children}</main>
-        {/* 푸터 */}
-        <Footer />
+        {showLayout && <Footer />}
       </body>
     </html>
   );
