6ff85fa45840b2a06b371fbc7ed439d0b6f31148
JHY <gusdud227@gmail.com>
2025-10-15 16:29:31 +0900

    feat: 헤더> 프로필 메뉴 선택 시, 내 정보 페이지로 이동

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,25 +1,28 @@
 'use client';
-
+import { useRouter } from 'next/navigation';
 import ProfileImage from '../ProfileImage/ProfileImage';
 
 export interface ProfileProps {
   userName?: string;
   userImage?: string;
-  onClick?: () => void;
 }
 
-export default function Profile({
-  userName,
-  userImage,
-  onClick,
-}: ProfileProps) {
+export default function Profile({ userName, userImage }: ProfileProps) {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push('/Profile/MyInfo'); // 프로필 메뉴로 클릭 시, 내정보 페이지로 이동
+  };
+
   return (
-    <div className="flex items-center gap-2 cursor-pointer" onClick={onClick}>
+    <div
+      className="flex items-center gap-2 cursor-pointer"
+      onClick={handleClick}
+    >
       {/* 프로필 이미지 */}
       <div className="h-8 w-8 rounded-full overflow-hidden">
         <ProfileImage imageUrl={userImage} name={userName} />
       </div>
-
       {/* 이름 */}
       <span className="text-[#1B1B1B] hover:text-[#79747E] font-medium text-sm leading-6 tracking-normal text-center transition-colors duration-300 whitespace-nowrap">
         {userName}
