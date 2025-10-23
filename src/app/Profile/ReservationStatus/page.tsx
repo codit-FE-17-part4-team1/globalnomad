e75f8be7dda5f7e6f72ccf95d006404938552f2a
yejinshim <simyejin9934@gmail.com>
2025-10-24 03:17:21 +0900

    브랜치 변경

--- src/app/Profile/ReservationStatus/page.tsx
+++ src/app/Profile/ReservationStatus/page.tsx
@@ -12,7 +12,7 @@ import useReservationsStatus from '@/hooks/useReservationsStatus';
 export default function ReservationStatusPage() {
   // 인증 기능 구현 후 실제 accessToken 연결 필요
   const accessToken =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjcwNCwidGVhbUlkIjoiMTctMSIsImlhdCI6MTc2MTI0Mjc1NywiZXhwIjoxNzYxMjQ0NTU3LCJpc3MiOiJzcC1nbG9iYWxub21hZCJ9.nSPtXQrDCDppTnMkT8DcqRM7C1-9t0D-QB8HAC41jBs';
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjcwNCwidGVhbUlkIjoiMTctMSIsImlhdCI6MTc2MTIzMzU5MSwiZXhwIjoxNzYxMjM1MzkxLCJpc3MiOiJzcC1nbG9iYWxub21hZCJ9.eHncKh8-9-EUTgZoWM7mcVqXqfuOykAEPP5SqzMNces';
   const {
     myActivities,
     isLoadingActivities,
