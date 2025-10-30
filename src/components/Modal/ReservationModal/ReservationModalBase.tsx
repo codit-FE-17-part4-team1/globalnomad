914f26ce0f97041f18b5bded656b1e6e0ca5d577
yejinshim <simyejin9934@gmail.com>
2025-10-31 04:28:06 +0900

    fix: 이미지 경로 수정

--- src/components/Modal/ReservationModal/ReservationModalBase.tsx
+++ src/components/Modal/ReservationModal/ReservationModalBase.tsx
@@ -245,7 +245,7 @@ export default function ReservationModalBase({
             {filteredReservations.length === 0 && (
               <div className="flex flex-col items-center justify-center py-8">
                 <Image
-                  src="/icon/earth.svg"
+                  src="/icon/images/design_2/earth.png"
                   alt="예약 내역 없음"
                   width={100}
                   height={100}
