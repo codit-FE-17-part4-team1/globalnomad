e5618f73af8cdfd74bf9516a3ba6ae34d76511ce
yejinshim <simyejin9934@gmail.com>
2025-10-31 19:54:47 +0900

    fix: BaseModal 내 이미지 삭제

--- src/components/Modal/BaseModal.tsx
+++ src/components/Modal/BaseModal.tsx
@@ -12,7 +12,6 @@ interface BaseModalProps {
   width?: string;
   closeOnOverlay?: boolean;
   closeOnEsc?: boolean;
-  type?: 'success' | 'error' | 'info'; // 타입 추가
 }
 
 export default function BaseModal({
@@ -24,20 +23,7 @@ export default function BaseModal({
   width,
   closeOnOverlay = true,
   closeOnEsc = true,
-  type = 'info', // 기본값
 }: BaseModalProps) {
-  const getIcon = () => {
-    switch (type) {
-      case 'success':
-        return '/images/design_2/success.png'; // 성공 이미지 경로
-      case 'error':
-        return '/images/design_2/warning.png'; // 실패 이미지 경로
-      case 'info':
-      default:
-        return '/images/design_2/earth.png'; // 기본 이미지 경로
-    }
-  };
-
   const overlayRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -81,13 +67,6 @@ export default function BaseModal({
             <h2 className="text-xl font-semibold mb-4">{title}</h2>
           </div>
         )}
-        <Image
-          src={getIcon()}
-          alt={`${type} icon`}
-          width={50}
-          height={50}
-          className="object-contain"
-        />
         {children}
       </div>
     </div>
