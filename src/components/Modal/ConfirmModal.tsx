922a2bb61d46c6db2453857d9e3ac5c027c92dc7
yejinshim <simyejin9934@gmail.com>
2025-10-31 19:48:48 +0900

    feat: BaseModal, ConfirmModal 내에 지구본 이미지 추가 (타입추가)

--- src/components/Modal/ConfirmModal.tsx
+++ src/components/Modal/ConfirmModal.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Image from 'next/image';
 import '@/styles/global.css';
 import BaseModal from '@/components/Modal/BaseModal';
 import MyButton from '@/components/Button/Button'; // 공통 컴포넌트로 변경 필요 (완료)
@@ -12,6 +13,7 @@ interface ConfirmModalProps {
   confirmLabel?: string; // 확인
   className?: string;
   size?: 'md' | 'lg' | 'xl';
+  type?: 'success' | 'error' | 'info'; // 타입 추가
 }
 
 export default function ConfirmModal({
@@ -21,13 +23,34 @@ export default function ConfirmModal({
   onConfirm,
   confirmLabel = '확인',
   className,
+  type = 'info', // 기본값
 }: ConfirmModalProps) {
+  const getIcon = () => {
+    switch (type) {
+      case 'success':
+        return '/images/design_2/success.png'; // 성공 이미지 경로
+      case 'error':
+        return '/images/design_2/warning.png'; // 실패 이미지 경로
+      case 'info':
+      default:
+        return '/images/design_2/earth.png'; // 기본 이미지 경로
+    }
+  };
+
   return (
     <BaseModal isOpen={isOpen} onClose={onClose} className={className}>
-      <div className="p-8">
-        <p className="pt-14 text-center text-black">{message}</p>
+      <div className="p-8  mt-10">
+        <div className="flex items-center justify-center mb-4">
+          <Image
+            src={getIcon()}
+            alt={`${type} icon`}
+            width={50}
+            height={50}
+            className="object-contain"
+          />
+        </div>
+        <p className="text-center text-black">{message}</p>
         <div className="mt-8 flex justify-end gap-2">
-          {/* 버튼 공통 컴포넌트 추가 위치, 현재 임시로 진행함  */}
           <MyButton
             color="buttonPrimary"
             onClick={onConfirm ?? onClose}
