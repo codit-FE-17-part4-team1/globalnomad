c4c543844dd163a7f151a58299fe5ca3afc05630
yejinshim <simyejin9934@gmail.com>
2025-11-01 02:02:06 +0900

    feat: 메타 태그 설정(타이틀, 오픈그래프)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,30 @@ import HeaderGate from '@/components/Header/HeaderGate';
 import HeaderServer from '@/components/Header/HeaderServer';
 import Footer from '@/components/Footer/Footer';
 
+export const metadata: Metadata = {
+  title: 'globalNomad',
+  description: '여행 체험을 등록하고 예약할 수 있는 플랫폼',
+  icons: {
+    icon: '/images/design_2/earth.png',
+  },
+
+  openGraph: {
+    title: 'globalNomad',
+    description:
+      'GlobalNomad는 사용자가 판매자와 체험자 모두 될 수 있는 체험 예약 플랫폼입니다.',
+    images: [
+      {
+        url: '/images/og-image.jpeg',
+        width: 1200,
+        height: 630,
+        alt: '5명이 으쌰으쌰',
+      },
+    ],
+    locale: 'ko_KR',
+    type: 'website',
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
